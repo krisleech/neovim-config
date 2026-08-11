380077903f989f48d9cbbf318a150fb43d97f632
Kris Leech <kris.leech@gmail.com>
2026-08-11 16:43:19 +0100

    tab for next buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,20 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>d", function() Snacks.bufdelete() end, { desc = "Delete Buffer" })
-vim.keymap.set("n", "<leader>D", function() Snacks.bufdelete.all() end, { desc = "Delete All Buffers" })
-vim.keymap.set("n", "<leader><space>", function() Snacks.picker.lines() end, { desc = "Find in Buffer" })
+vim.keymap.set("n", "<leader>d", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>D", function()
+  Snacks.bufdelete.all()
+end, { desc = "Delete All Buffers" })
+vim.keymap.set("n", "<leader><space>", function()
+  Snacks.picker.lines()
+end, { desc = "Find in Buffer" })
 vim.keymap.set("n", "<leader><leader>", "<C-^>", { desc = "Switch to Last Buffer" })
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous Buffer" })
+-- vim.keymap.set("n", "<leader>ai", function()
+--   require("config.llm_reference").send_current_location()
+-- end, { desc = "Send File Location to LLM" })
 vim.keymap.set("n", "n", "nzz", { desc = "Next Result (centered)" })
 vim.keymap.set("n", "N", "Nzz", { desc = "Prev Result (centered)" })
